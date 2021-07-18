207e6f76bd2e202a5de28a4d2029cd8751126c1a
muneeshdk <45361298+muneeshdk@users.noreply.github.com>
2021-07-18 17:31:26 +0530

    Update MainFile.py
    Checking with USA and NATO acronyms.

--- MainFile.py
+++ MainFile.py
@@ -10,7 +10,11 @@ def translate(w):
     w=w.lower()
     if w in data:
         return data[w]
-
+    elif w.title() in data:             #if user entered delhi then check
+        return data[w.title()]
+    elif w.upper() in data:             #if user entered USA or NATO then check
+        return data[w.upper()]
+    
     elif len(get_close_matches(w,data.keys()))>0:
         # if similar word is added like rain and rainn then choose right one 
         yn=input("Did you mean %s instead? Enter y if yes or n if no "%get_close_matches(w,data.keys())[0])
